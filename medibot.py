ec6ddfa51565a1c5e31f40b2ba301372ecf9ec11
PrasadSutar08 <prasadravindrasutar@gmail.com>
2025-04-03 17:52:43 +0530

    evawv

--- medibot.py
+++ medibot.py
@@ -5,86 +5,91 @@ from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEndpoint
-from dotenv import load_dotenv, find_dotenv
 
+## Uncomment the following files if you're not using pipenv as your virtual environment manager
+from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-DB_FAISS_PATH = "vectorstore/db_faiss"
 
+DB_FAISS_PATH="vectorstore/db_faiss"
 @st.cache_resource
 def get_vectorstore():
-    embedding_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-    db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
     return db
 
+
 def set_custom_prompt(custom_prompt_template):
-    prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
     return prompt
 
+
 def load_llm(huggingface_repo_id, HF_TOKEN):
-    llm = HuggingFaceEndpoint(
+    llm=HuggingFaceEndpoint(
         repo_id=huggingface_repo_id,
         temperature=0.8,
-        model_kwargs={"token": HF_TOKEN, "max_length": "512"}
+        model_kwargs={"token":HF_TOKEN,
+                      "max_length":"512"}
     )
     return llm
+
 
 def main():
     st.title("Ask Medical Chatbot!")
     st.text("This custom GPT leverages a combination of advanced AI along an Encyclopedia entrusted by experienced Doctors to provide accurate, fact-based medical solutions.")
     st.text("Copyright © 2025 Prasad Sutar. All rights reserved.")
-    
     if 'messages' not in st.session_state:
         st.session_state.messages = []
-    
-    conversation_history = "".join([f"{msg['role']}: {msg['content']}\n" for msg in st.session_state.messages])
 
     for message in st.session_state.messages:
         st.chat_message(message['role']).markdown(message['content'])
 
-    prompt = st.chat_input("How can I assist you with your concern?")
+    prompt=st.chat_input("How can I assist you with your concern?")
 
     if prompt:
         st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role': 'user', 'content': prompt})
+        st.session_state.messages.append({'role':'user', 'content': prompt})
 
-        CUSTOM_PROMPT_TEMPLATE = f"""
-            Use the pieces of information provided in the context and previous conversation to answer the user's question.
-            If you don't know the answer, just say that you don't know. Don't try to make up an answer. 
-            Don't provide anything out of the given context.
-            
-            Previous Conversation:
-            {conversation_history}
-            
-            Context: {{context}}
-            Question: {{question}}
+        CUSTOM_PROMPT_TEMPLATE = """
+                Use the pieces of information provided in the context to answer user's question.
+                If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+                Dont provide anything out of the given context
 
-            Start the answer directly. No small talk please.
-        """
+                Context: {context}
+                Question: {question}
 
-        HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
-        HF_TOKEN = os.environ.get("HF_TOKEN")
+                Start the answer directly. No small talk please.
+                """
+        
+        HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
+        HF_TOKEN=os.environ.get("HF_TOKEN")
 
-        try:
-            vectorstore = get_vectorstore()
+        try: 
+            vectorstore=get_vectorstore()
             if vectorstore is None:
                 st.error("Failed to load the vector store")
 
-            qa_chain = RetrievalQA.from_chain_type(
+            qa_chain=RetrievalQA.from_chain_type(
                 llm=load_llm(huggingface_repo_id=HUGGINGFACE_REPO_ID, HF_TOKEN=HF_TOKEN),
                 chain_type="stuff",
-                retriever=vectorstore.as_retriever(search_kwargs={'k': 3}),
+                retriever=vectorstore.as_retriever(search_kwargs={'k':3}),
                 return_source_documents=True,
-                chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+                chain_type_kwargs={'prompt':set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
             )
 
-            response = qa_chain.invoke({'query': prompt})
-            result = response["result"]
-            st.chat_message('assistant').markdown(result)
-            st.session_state.messages.append({'role': 'assistant', 'content': result})
+            response=qa_chain.invoke({'query':prompt})
+
+            result=response["result"]
+            source_documents=response["source_documents"]
+            result_to_show=result
+            #response="Hi, I am MediBot!"
+            st.chat_message('assistant').markdown(result_to_show)
+            st.session_state.messages.append({'role':'assistant', 'content': result_to_show})
 
         except Exception as e:
             st.error(f"Error: {str(e)}")
+
+            
 
 if __name__ == "__main__":
     main()
